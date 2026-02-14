b7c125a882fbbd1f7210a59b91d68b9c43281d43
nagcas <studio.nagcas@outlook.it>
2026-02-14 13:00:04 +0100

    Implement thank you state and update rating submission logic

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-// Verifico se il mio script è collegato correttamente al document html
+// I check if my script is linked correctly to the html document
 console.log("It's working...");
 
 const rating = document.querySelectorAll('.rating-start__item-selected');
-console.log(rating);
+const submit = document.getElementById('button-submit');
+const ratingStart = document.querySelector('.rating-start');
+const thankYouVisibled = document.querySelector('.visibled__off');
+const ratingValue = document.querySelector('.rating-value');
+
+let currentSelected = null;
 
 for (let i = 0; i < rating.length; i++) {
   rating[i].addEventListener('click', () => {
-    console.log(rating[i]);
-    rating[i].classList.add('selected');
+    // Removes the previous selection
+    for (let i = 0; i < rating.length; i++) {
+      rating[i].classList.remove('selected-item');
+    }
+    
+    // Select the current rating
+    rating[i].classList.add('selected-item');    
+    
+    // Assigns the selected value to the variable
+    currentSelected = rating[i].innerText;
+    
+    // Re-enable the submit button
+    submit.disabled = false;
   })
 }
+
+submit.addEventListener('click', () => {
+  // Display rating start none
+  ratingStart.style.display = 'none';
+
+  // display thank you visibled on
+  thankYouVisibled.classList.remove('visibled__off');
+  thankYouVisibled.classList.add('visibled__on');
+
+  // Add value rating
+  ratingValue.innerText = currentSelected;
+})
